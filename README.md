49d7870440363d4b484b6f1f5312f8c69e712e15
Patrick Holl <me@patrick-holl.com>
2016-05-01 15:05:36 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# maien
+
+A simple module to turn the meta information of a website to a JavaScript object. The properties of the resulting object refer directly to the values of the following attributes:
+
+``` js
+['name', 'http-equiv', 'rel', 'property', 'itemprop']
+```
+
+Here is a nice list of what you can expect as meta information in a web site: https://github.com/joshbuchea/HEAD
+
+#### Example
+``` js
+var siteObject = maien('https://www.sitepoint.com', function(data) {
+ console.log(data);
+});
+
+// This will result in:
+{ meta:
+   { viewport: 'width=device-width, initial-scale=1.0',
+     description: 'Learn Web Design & Development with SitePoint tutorials, courses and books - HTML5, CSS3, JavaScript, PHP, mobile app development, Responsive Web Design',
+     robots: 'noodp',
+     twitter:card: 'summary',
+     twitter:description: 'Learn Web Design & Development with SitePoint tutorials, courses and books - HTML5, CSS3, JavaScript, PHP, mobile app development, Responsive Web Design',
+     twitter:title: 'SitePoint – Learn HTML, CSS, JavaScript, PHP, Ruby & Responsive Design',
+     twitter:site: '@sitepointdotcom',
+     generator: 'WordPress 4.4.2',
+     wp-parsely_version: '1.8' } }
+
+```
--- index.js
+++ index.js
@@ -1,84 +1,88 @@
+"use strict";
+
 var request = require('request');
 var cheerio = require('cheerio');
 
+/**
+ * maien
+ * A simple module to turn meta information of a website to a JavaScript object
+ * 
+ * 
+ * Example:
+ * var siteObject = maien('http://heise.de', function(data) {
+ *	console.log(data);
+ * });
+ *
+ * 
+ * @param  {String}   url The page url
+ * @param  {Function} cb  The callback function
+ * @return {Tinyreq} The request object.
+ */
+function maien(url, cb) {
 
-request('http://www.gamestar.de/spiele/team-fortress-2/news/team_fortress_2,43542,3271824.html', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  	var $ = cheerio.load(body,{ normalizeWhitespace: false, xmlMode: false, decodeEntities: true, lowerCaseAttributeNames : true });
-  	var headAttributes = ['name', 'http-equiv', 'rel', 'property', 'itemprop'];
+	var siteObj = {meta: {}};
 
+	return request(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	var $ = cheerio.load(body,{ normalizeWhitespace: false, xmlMode: false, decodeEntities: true, lowerCaseAttributeNames : true });
+	  	var headAttributes = ['name', 'http-equiv', 'rel', 'property', 'itemprop'];
 
+	  	var head = $('head meta');
+	  	var index, len, attr, val;
+	  	$('head meta').each(function(index, elem) {
 
-  	var head = $('head meta');
-  	var siteObj = {meta: {}, img: {}};
+	  		for (index = 0, len = headAttributes.length; index < len; ++index) {
+			    attr = $(this).attr(headAttributes[index]);
 
-  	var index, len, attr, val;
-  	$('head meta').each(function(index, elem) {
+			    if(typeof attr === 'undefined') {
+			    	return true;
+			    }
 
-  		for (index = 0, len = headAttributes.length; index < len; ++index) {
-		    attr = $(this).attr(headAttributes[index]);
+			    attr = String.prototype.toLowerCase.apply(attr);
 
-		    if(typeof attr === 'undefined') {
-		    	return true;
-		    }
+			    switch(headAttributes[index]) {
+			    	case 'rel':
+			    		val = $(this).attr('href');
+			    		break;
+			    	case 'name':
+			    		val = $(this).attr('content');
+			    		break;
+			    	case 'http-equiv':
+			    		val = $(this).attr('content');
+			    		break;
+			    	case 'property':
+			    		val = $(this).attr('content');
+			    		break;
+			    	case 'itemprop':
+			    		val = $(this).attr('property');
+			    		break;
+			    	default:
+			    		val = $(this).attr('content');
+			    		break;
+			    }
 
-		    attr = String.prototype.toLowerCase.apply(attr);
+			    switch(attr) {
+			    	case 'keywords':
+			    		val = val.replace(/[\s,]+/g, ',').split(',');
+			    		break;
+			    	case 'date':
+			    		val = new Date(val);
+			    		if(val === 'Invalid Date') {
+			    			val = val;
+			    		}
+			    		break;
+			    	default:
+			    		break;
+			    }
+			    siteObj['meta'][attr] = val;
+			}
+	  	});
 
-		    switch(headAttributes[index]) {
-		    	case 'rel':
-		    		val = $(this).attr('href');
-		    		break;
-		    	case 'name':
-		    		val = $(this).attr('content');
-		    		break;
-		    	case 'http-equiv':
-		    		val = $(this).attr('content');
-		    		break;
-		    	case 'property':
-		    		val = $(this).attr('content');
-		    		break;
-		    	case 'itemprop':
-		    		val = $(this).attr('property');
-		    		break;
-		    	default:
-		    		val = $(this).attr('content');
-		    		break;
-		    }
+	  	// Invoke callback function
+	  	cb(siteObj);
 
-		    switch(attr) {
-		    	case 'keywords':
-		    		val = val.split(',');
-		    		break;
-		    	case 'date':
-		    		val = new Date(val);
-		    		if(val === 'Invalid Date') {
-		    			val = val;
-		    		}
-		    		break;
-		    	default:
-		    		break;
-		    }
-
-
-
-		    siteObj['meta'][attr] = val;
-
-		}
-
-  	});
-
-
-  	$('img').each(function(index, elem) {
-  		// siteObj['img'][index] = $(this).attr('src');
-  	});
-
-
-  	var comment = $('#comment .commentText').text();
-
-  	console.log(JSON.stringify(siteObj));
-  	// console.log(comment);
-
-  	//console.log(head);
-
-  }
-})
+	  	// Request successful
+	  }
+	});
+	// eof	
+}
